ff94d94c219729e183cf520304e161c4afb5b0de
Mickael Labau <mickael.labau@gmail.com>
2025-05-17 23:58:25 +0200

    Fix compilation

--- OctoWhirl.GUI/ViewModels/Technical/TabItemViewModel.cs
+++ OctoWhirl.GUI/ViewModels/Technical/TabItemViewModel.cs
@@ -4,18 +4,6 @@ namespace OctoWhirl.GUI.ViewModels.Technical
 {
     public class TabItemViewModel : BaseViewModel
     {
-        private string _header;
-        public string Header
-        {
-            get => _header;
-            set => SetProperty(ref _header, value);
-        }
-
-        private object _content;
-        public object Content
-        {
-            get => _content;
-            set => SetProperty(ref _content, value);
-        }
+        public required object Content { get; init; }
     }
 }
