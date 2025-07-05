e177f633a4ef6225da4c84d758bc450eae4ea00d
Antoine Herrmamm <117520250+AntoineHerrmannGit@users.noreply.github.com>
2025-07-05 23:23:18 +0200

    [Services] [Data] CorporateActions from YahooFinance

---
+++ OctoWhirl.Core/Models/Common/CorporateAction.cs
@@ -0,0 +1,9 @@
+﻿namespace OctoWhirl.Core.Models.Common
+{
+    public abstract class CorporateAction
+    {
+        public abstract CorporateActionType ActionType { get; }
+        public DateTime TimeStamp { get; set; }
+        public string Reference { get; set; }
+    }
+}
