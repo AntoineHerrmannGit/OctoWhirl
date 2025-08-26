ca6cbb7cb8171137affcc2a7be0d211509a7a3ae
Antoine Herrmann <antoine67.herrmann@laposte.net>
2025-08-26 22:37:13 +0200

    [GUI] Continued but still fails....

---
+++ GUI/OctoWhirl/Converters/ThemeIconConverer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using System.Windows;
+
+namespace OctoWhirl.GUI.Converters
+{
+    public class ThemeIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // value = nom de l'icône (ex: "home.png")
+            // parameter = optionnel, peut contenir le sous-dossier (ex: "Icons")
+
+            if (value is string iconName)
+            {
+                // Récupère le thème courant depuis les ressources
+                string theme = Application.Current.Resources["CurrentTheme"] as string ?? "DarkTheme";
+
+                // Chemin complet
+                string folder = parameter as string ?? "Images";
+                string path = $"pack://application:,,,/Ressources/{folder}/{theme}/{iconName}";
+
+                return new BitmapImage(new Uri(path, UriKind.Absolute));
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
