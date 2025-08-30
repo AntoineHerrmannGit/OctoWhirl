6496406144fc656e7ba00612b13c66097732e7e3
Antoine Herrmamm <117520250+AntoineHerrmannGit@users.noreply.github.com>
2025-08-30 11:43:26 +0200

    GUI/main window (#50)
    * [GUI) Started building main window
    
    * [GUI] Continued but still fails....
    
    * [GUI] Set ressources as PreserveNewest
    
    * [GUI] Fixed ressource import but still does not show images
    
    * [GDA] Fixed csproj
    
    * [GUI] Setup OctoWhirl MainWindow and WelcomeView (but still does not compile the CardButtonStyle....)
    
    * [GUI] Fixed carding but still can't import CardButtonStyle (namespace bug)
    
    * [GUI] Fixed assembly imports -> This works !

---
+++ GUI/OctoWhirl/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using OctoWhirl.GUI.Views;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OctoWhirl.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Take into account already loaded views and remembers when lost focus
+        private Dictionary<string, UserControl> _activeViews;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _activeViews = new Dictionary<string, UserControl>() { { "Home", new WelcomeView() } };
+            MainContent.Content = _activeViews["Home"];
+        }
+
+        #region Private Navigation within views
+        private void NavButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.Content is string viewName)
+            {
+                NavigateTo(viewName);
+            }
+        }
+
+        public void NavigateTo(string viewName)
+        {
+            switch (viewName)
+            {
+                case "Home":
+                    if (_activeViews.ContainsKey(viewName))
+                        MainContent.Content = _activeViews["Home"];
+                    else
+                        MainContent.Content = new WelcomeView();
+                    break;
+                case "Risk Analysis":
+                    if (_activeViews.ContainsKey(viewName))
+                        MainContent.Content = _activeViews["Risk"];
+                    else
+                        MainContent.Content = new RiskView();
+                    break;
+                case "Trading":
+                    if (_activeViews.ContainsKey(viewName))
+                        MainContent.Content = _activeViews["Trading"];
+                    else
+                        MainContent.Content = new TradingView();
+                    break;
+                case "Settings":
+                    if (_activeViews.ContainsKey(viewName))
+                        MainContent.Content = _activeViews["Settings"];
+                    else
+                        MainContent.Content = new SettingsView();
+                    break;
+            }
+        }
+        #endregion Private Navigation within views
+    }
+}
---
+++ GUI/OctoWhirl/Ressources/Styles/Custom/CardButton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace OctoWhirl.GUI.Ressources.Styles.Custom
+{
+    public class CardButton : Button
+    {
+        public static readonly DependencyProperty CardTitleProperty =
+            DependencyProperty.Register("CardTitle", typeof(string), typeof(CardButton), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty CardDescriptionProperty =
+            DependencyProperty.Register("CardDescription", typeof(string), typeof(CardButton), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty IconSourceProperty =
+            DependencyProperty.Register("IconSource", typeof(ImageSource), typeof(CardButton), new PropertyMetadata(null));
+
+        public string CardTitle
+        {
+            get => (string)GetValue(CardTitleProperty);
+            set => SetValue(CardTitleProperty, value);
+        }
+
+        public string CardDescription
+        {
+            get => (string)GetValue(CardDescriptionProperty);
+            set => SetValue(CardDescriptionProperty, value);
+        }
+
+        public ImageSource IconSource
+        {
+            get => (ImageSource)GetValue(IconSourceProperty);
+            set => SetValue(IconSourceProperty, value);
+        }
+    }
+}
