ba1f4ceb37bb86df7dc55e78b4b7bf724d62c6db
Antoine Herrmamm <117520250+AntoineHerrmannGit@users.noreply.github.com>
2025-09-07 13:41:08 +0200

    Core/cache in memory (#53)
    * [Core] Added In-memory cache
    
    * [Core] [Cache] tests

---
+++ Core/Cache/ICache.cs
@@ -0,0 +1,14 @@
+﻿namespace OctoWhirl.Core.Cache
+{
+    public interface ICache
+    {
+        T Get<T>(string key);
+        Dictionary<string, T> Get<T>(IEnumerable<string> keys);
+
+        bool Set<T>(string key, T value, bool _override = false);
+        bool Set<T>(Dictionary<string, T> values, bool _override = false);
+        bool Remove(string key);
+        bool Remove(IEnumerable<string> keys);
+        bool Flush();
+    }
+}
--- Core/Tools/Serializer/SerializationExtension.cs
+++ Core/Tools/Serializer/SerializationExtension.cs
@@ -1,10 +1,40 @@
 ﻿using Newtonsoft.Json;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
 
 namespace OctoWhirl.Core.Tools.Serializer
 {
     public static class SerializationExtension
     {
+        private static readonly Encoding _encode = Encoding.UTF8;
+
+        #region Serialization
         public static string SerializeToJson(this object obj) => JsonConvert.SerializeObject(obj);
+        public static byte[] SerializeToJsonAndCompress(this object obj)
+        {
+            string jsonString = obj.SerializeToJson();
+            byte[] jsonBytes = _encode.GetBytes(jsonString);
+
+            using (var outputStream = new MemoryStream())
+            {
+                using (var gzipStream = new GZipStream(outputStream, CompressionMode.Compress))
+                    gzipStream.Write(jsonBytes, 0, jsonBytes.Length);
+                return outputStream.ToArray();
+            }
+        }
+        #endregion Serialization
+
+        #region Deserialization
         public static TResponse DeserializeFromJson<TResponse>(this string json) => JsonConvert.DeserializeObject<TResponse>(json);
+        public static T DecompressAndDeserializeFromJson<T>(this byte[] compressedData)
+        {
+            using (var inputStream = new MemoryStream(compressedData))
+            using (var gzipStream = new GZipStream(inputStream, CompressionMode.Decompress))
+            using (var reader = new StreamReader(gzipStream, _encode))
+                return reader.ReadToEnd().DeserializeFromJson<T>();
+        }
+
+        #endregion Deserialization
     }
 }
