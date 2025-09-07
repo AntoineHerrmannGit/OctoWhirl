60f6299e12537f889f40d94b11c3d1e6cc9451d4
Antoine Herrmann <antoine67.herrmann@laposte.net>
2025-09-07 12:55:40 +0200

    [Core] Added In-memory cache

---
+++ Core/Cache/CacheInMemory/CacheInMemory.cs
@@ -0,0 +1,73 @@
+﻿using OctoWhirl.Core.Tools.Serializer;
+
+namespace OctoWhirl.Core.Cache.CacheInMemory
+{
+    public class CacheInMemory : ICache
+    {
+        private Dictionary<string, byte[]> _container;
+        
+        public CacheInMemory()
+        {
+        }
+
+        #region Get
+        public T Get<T>(string key)
+        {
+            if (_container.ContainsKey(key))
+                return _container[key].DecompressAndDeserializeFromJson<T>();
+            return default(T);
+        }
+        public Dictionary<string, T> Get<T>(IEnumerable<string> keys)
+            => keys.AsParallel().ToDictionary(
+                key => key,
+                key => Get<T>(key)
+            );
+        #endregion Get
+
+        #region Set
+        public bool Set<T>(string key, T value, bool _override = false)
+        {
+            if(!_override && _container.ContainsKey(key))
+                return false;
+            _container[key] = value.SerializeToJsonAndCompress();
+            return true;
+        }
+
+        public bool Set<T>(Dictionary<string, T> values, bool _override = false)
+        {
+            var buffer = new Dictionary<string, byte[]>();
+            var results = new List<bool>();
+            foreach(var kv in values)
+            {
+                if (_container.ContainsKey(kv.Key))
+                    buffer[kv.Key] = _container[kv.Key];
+                results.Add(Set(kv.Key, kv.Value, _override));
+            }
+
+            if(results.Any(r => !r))
+            {
+                foreach(var kv in buffer)
+                    _container[kv.Key] = kv.Value;
+                return false;
+            }
+            return true;
+        }
+        #endregion Set
+
+        #region Remove
+        public bool Remove(string key) 
+            => _container.ContainsKey(key) && _container.Remove(key);
+
+        public bool Remove(IEnumerable<string> keys) 
+            => keys.All(key => _container.ContainsKey(key) &&  _container.Remove(key));
+        #endregion Remove
+
+        #region Flush
+        public bool Flush()
+        {
+            _container = new Dictionary<string, byte[]>();
+            return true;
+        }
+        #endregion Flush
+    }
+}
--- Core/Tools/Serializer/SerializationExtension.cs
+++ Core/Tools/Serializer/SerializationExtension.cs
@@ -1,10 +1,38 @@
 ﻿using Newtonsoft.Json;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
 
 namespace OctoWhirl.Core.Tools.Serializer
 {
     public static class SerializationExtension
     {
+        #region Serialization
         public static string SerializeToJson(this object obj) => JsonConvert.SerializeObject(obj);
+        public static byte[] SerializeToJsonAndCompress(this object obj)
+        {
+            string jsonString = obj.SerializeToJson();
+            byte[] jsonBytes = Encoding.UTF8.GetBytes(jsonString);
+
+            using (var outputStream = new MemoryStream())
+            using (var gzipStream = new GZipStream(outputStream, CompressionLevel.Optimal, leaveOpen: true))
+            {
+                gzipStream.Write(jsonBytes, 0, jsonBytes.Length);
+                return outputStream.ToArray();
+            }
+        }
+        #endregion Serialization
+
+        #region Deserialization
         public static TResponse DeserializeFromJson<TResponse>(this string json) => JsonConvert.DeserializeObject<TResponse>(json);
+        public static T DecompressAndDeserializeFromJson<T>(this byte[] compressedData)
+        {
+            using (var inputStream = new MemoryStream(compressedData))
+            using (var gzipStream = new GZipStream(inputStream, CompressionMode.Decompress))
+            using (var reader = new StreamReader(gzipStream, Encoding.UTF8))
+                return reader.ReadToEnd().DeserializeFromJson<T>();
+        }
+
+        #endregion Deserialization
     }
 }
